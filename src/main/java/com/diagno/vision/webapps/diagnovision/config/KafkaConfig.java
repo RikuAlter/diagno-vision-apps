df22729a5a46f899fc75b45894dce3369a763d90
RikuAlter <rikuAlter@gmail.com>
2023-07-05 06:48:48 +0530

    [DVW-03] Added kafka config

--- src/main/java/com/diagno/vision/webapps/diagnovision/config/KafkaConfig.java
+++ src/main/java/com/diagno/vision/webapps/diagnovision/config/KafkaConfig.java
@@ -1,12 +1,28 @@
 package com.diagno.vision.webapps.diagnovision.config;
 
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.ByteArraySerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.Map;
+import java.util.Properties;
 
 @Configuration
 public class KafkaConfig {
 
     @Bean
-    public ProducerFactory<String, Image>
+    public ProducerFactory<String, byte[]> producerFactory(KafkaProperties kafkaProperties){
+        return new DefaultKafkaProducerFactory<>(kafkaProperties.buildProducerProperties(), new StringSerializer(), new ByteArraySerializer());
+    }
+
+    @Bean
+    public KafkaTemplate<String, byte[]> kafkaTemplate(KafkaProperties kafkaProperties){
+        return new KafkaTemplate<>(producerFactory(kafkaProperties));
+    }
 }
